93dcaf5f8e25720d9b6eee0e02fb819b58586ac2
antoinewdg <antoinewendlinger@gmail.com>
2017-05-11 18:35:12 +0200

    Add benchmark comparison

--- rbu/benchmark.py
+++ rbu/benchmark.py
@@ -17,3 +17,16 @@ def benchmark_commits(commits, repo, output_dir, warmup=True):
         repo.git.checkout(sha)
         cmd = 'cargo bench > {}/{}.txt'.format(output_dir, sha)
         subprocess.check_output(cmd, shell=True)
+
+
+def compare_benchmarks(base_sha, others_sha, benchmark_dir):
+    comparisons = {}
+    for sha in others_sha:
+        cmd = 'cargo benchcmp {dir}/{sha_1}.txt {dir}/{sha_2}.txt --threshold 2 --variance' \
+            .format(dir=benchmark_dir, sha_1=base_sha, sha_2=sha)
+        out = subprocess.check_output(cmd, shell=True).decode('utf-8')
+        if out == '':
+            out = 'no significant variations'
+        comparisons[sha] = out
+
+    return comparisons
--- test/test_benchmark.py
+++ test/test_benchmark.py
@@ -1,10 +1,10 @@
 import tempfile
 import glob
-from os.path import join
+from os.path import join, dirname
 
 import pytest
 
-from rbu.benchmark import benchmark_commits
+from rbu.benchmark import benchmark_commits, compare_benchmarks
 
 COMMITS = {
     '404ddb8': 'Changes nothing',
@@ -20,3 +20,11 @@ def test_benchmark_commits(setup_repo):
         result = glob.glob(join(directory, '*.txt'))
         for sha in COMMITS:
             assert join(directory, sha + '.txt') in result
+
+
+def test_compare_benchmarks():
+    assets_dir = join(dirname(__file__), 'assets', 'benchmark_outputs')
+    comparisons = compare_benchmarks('24ec188', ['404ddb8', 'aa3edfe'], assets_dir)
+
+    assert comparisons['aa3edfe'] != 'no significant variations'
+    assert comparisons['404ddb8'] == 'no significant variations'
